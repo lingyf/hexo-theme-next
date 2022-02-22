43cbd06037ae7804a2798c975e1304069d0a8e2d
Albert <lingyf@hotmail.com>
2022-02-22 15:04:49 +0800

    feat: reduce animation duration

--- source/js/motion.js
+++ source/js/motion.js
@@ -18,7 +18,7 @@ NexT.motion.integrator = {
     if (!CONFIG.motion.async) this.queue = [this.queue];
     this.queue.forEach(sequence => {
       const timeline = window.anime.timeline({
-        duration: 200,
+        duration: 20,
         easing  : 'linear'
       });
       sequence.forEach(item => {
@@ -37,8 +37,8 @@ NexT.motion.middleWares = {
       sequence.push({
         targets,
         scaleX  : [0, 1],
-        duration: 500,
-        deltaT  : '-=200'
+        duration: 60,
+        deltaT  : '-=20'
       });
     }
 
@@ -47,7 +47,7 @@ NexT.motion.middleWares = {
         targets,
         opacity: 1,
         top    : 0,
-        deltaT : sequenceQueue ? '-=200' : '-=0'
+        deltaT : sequenceQueue ? '-=20' : '-=0'
       });
     }
 
@@ -63,7 +63,7 @@ NexT.motion.middleWares = {
       sequence.push({
         targets,
         complete: () => targets.classList.add('animated', 'fadeInDown'),
-        deltaT  : '-=200'
+        deltaT  : '-=20'
       });
     });
 
@@ -90,7 +90,7 @@ NexT.motion.middleWares = {
         sequence.push({
           targets,
           complete: () => targets.classList.add('animated', animation),
-          deltaT  : '-=100'
+          deltaT  : '-=10'
         });
       });
     }
